24a736e3138854eb386616edac15b17642f1e343
Andrés <andresconsani03@gmail.com>
2024-07-10 10:14:14 -0400

    Montando servidor web

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,3 +1,16 @@
-import express from 'express'
-let server = express();
-console.log('hola');
+import express from 'express';
+const server = express();
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import path from 'path';
+
+server.use(bodyParser.raw({ type: 'application/octet-stream', limit: '500mb' }));
+server.use(express.json({ limit: '500mb' }));
+server.use(cors());
+server.use(express.urlencoded({ extended: true }));
+
+server.use(express.static(path.join(import.meta.dirname, '..', 'assets', 'views')));
+
+server.listen(8080, function(){
+    console.log("- - - ONLINE - - -");
+})
